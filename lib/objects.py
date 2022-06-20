2771144224e04f4a62deadd829ee06c27a6e9cec
Felipe-RA <felipeangel50@gmail.com>
2022-06-20 16:13:15 -0500

    implementation of a block with sanity checks

--- lib/objects.py
+++ lib/objects.py
@@ -10,15 +10,15 @@ class HashedNode:
     def __init__(self, input_value) -> object:
         self.left_leave = None
         self.right_leave = None
+        self.input_value = input_value
         self.hashed_value = hashlib.sha256(input_value.encode("utf-8")).hexdigest()
 
     def __repr__(self) -> str:
         return "left leave: " + str(self.left_leave) + "right leave: " + str(self.right_leave) \
-             + "Hash: " + str(self.hashed_value)
+             + "Hash: " + str(self.hashed_value) + "Input Value:" + self.input_value
 
     def __str__(self) -> str:
-        return str(self.left_leave) + str(self.right_leave) +  str(self.hashed_value)
-
+        return str(self.left_leave) + str(self.right_leave) + str(self.input_value) + str(self.hashed_value) 
 
 class Transaction:
     """
@@ -62,3 +62,65 @@ class Transaction:
 
     def __str__(self) -> str:
         return str(self.user_1_public_key) + str(self.inputs) + str(self.outputs) + str(self.prev_transaction_hash)
+
+class MetadataBlockchain:
+    used_nonces = []
+    def __init__(self) -> None:
+        pass
+    
+    @classmethod
+    def is_valid_nonce(nonce_to_check):
+        if nonce_to_check[0:4] != "0000":
+            return False
+        if nonce_to_check in MetadataBlockchain.used_nonces:
+            return False
+        return True
+
+
+
+
+class Block:
+    """
+    A Block that contains all the information required to assert proof of work.
+
+    Contrary to the suggestion of Satoshi, the difficulty of our Nonce will be fixed to 4 leading zeros
+    instead of using a moving average. This is for the sake of simplicity.
+    """
+
+    def __init__(self, merkle_root: HashedNode, nonce: str, previous_block = '0', is_genesis_block = False, transactions_per_block = 4) -> object:
+        """
+        previous_block_hash will initialize as a '0'  for the first instance of the program
+        is_genesis_block: a boolean flag to indicate the first object
+        transactions_pero_block: an integer with the predefined number of transactions.
+
+        we check that the nonce is correct by applying sha256 and checking the number of leading zeros
+        """ 
+
+        ## sanity checks
+        if previous_block == '0' and is_genesis_block == False:
+            raise ValueError("Detected incorrect input for previous block on non genesis block")
+
+        nonce_to_check = hashlib.sha256(nonce.encode("utf-8")).hexdigest()
+
+        if not MetadataBlockchain.is_valid_nonce(nonce_to_check):
+            raise ValueError("Nonce is not correct!")
+        ## end sanity checks
+
+        self.merkle_root = merkle_root
+        self.nonce = nonce
+
+        if is_genesis_block:
+            self.previous_block_hash = previous_block
+        else:
+            self.previous_block_hash = hashlib.sha256(str(previous_block).encode("utf-8")).hexdigest()
+
+
+    def __repr__(self) -> str:
+        return "Block Info: " + "\nPrevious Block Hash:\n" + self.previous_block_hash + \
+            "\nMerkle Root Hash:\n" + self.merkle_root.hashed_value + \
+            "\nNonce: " + self.nonce
+
+    def __str__(self) -> str:
+        return self.previous_block_hash + self.merkle_root.hashed_value + self.nonce
+
+        
