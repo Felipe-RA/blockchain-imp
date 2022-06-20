ea60de885a3c86195790703965d52d207a9d560b
Felipe-RA <felipeangel50@gmail.com>
2022-06-20 13:22:42 -0500

    added some documentation

--- lib/objects.py
+++ lib/objects.py
@@ -1,9 +1,52 @@
 import hashlib
 
 class HashedNode:
+    """
+    An implementation of a Merkle Node using sha256 hashing.
+    Values are utf-8 forced before hashing.
+    
+    Attributes are self explanatory
+    """
     def __init__(self, input_value) -> object:
         self.left_leave = None
         self.right_leave = None
-        self.input_value = input_value
         self.hashed_value = hashlib.sha256(input_value.encode("utf-8")).hexdigest()
 
+    def __str__(self) -> str:
+        return "left leave: " + str(self.left_leave) + "right leave: " + str(self.right_leave) \
+             + "Hash: " + str(self.hashed_value)
+
+
+class Transaction:
+    """
+    A transaction between two parties, user0 and user1
+    """
+    
+    def __init__(self, user_1_public_key: str, inputs = [], outputs = [], prev_transaction_hash = '0' ,genesis = False) -> object:
+
+        """
+        Constructor for a transaction.
+        
+        Params:
+        user_1_public_key: A string with the provided public key from the user0
+        inputs: A list with the coins that are entering the transaction
+        outputs: A list with the coins that are exiting the transaction (for multiple outputs, and change if any)
+        prev_transaction_hash: A string with the hashed value of the previous transaction. Default is '0' for the first transaction ever.
+        genesis: A boolean flag to mark if a transaction is the first one
+        """
+
+        self.user_1_public_key = user_1_public_key
+        self.inputs = inputs
+        self.outputs = outputs
+        self.prev_transaction_hash = prev_transaction_hash
+
+        if genesis:
+            user_0_signature = '0'
+            concat_string = str(self.user_1_public_key) + str(prev_transaction_hash)
+            concat_string = hashlib.sha256(concat_string.encode("utf-8")).hexdigest() + user_0_signature
+            self.user_0_signature = hashlib.sha256(concat_string.encode("utf-8")).hexdigest()
+        else:
+            user_0_signature = input("Input your signature: ")
+            concat_string = str(self.user_1_public_key) + str(prev_transaction_hash)
+            concat_string = hashlib.sha256(concat_string.encode("utf-8")).hexdigest() + user_0_signature
+            self.user_0_signature = hashlib.sha256(concat_string.encode("utf-8")).hexdigest()
